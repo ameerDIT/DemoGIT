5500479644b3d2c6b164d1eafab1f45a7928d7cc
ameerDIT <ameer.shaik@dharani.co.in>
2022-12-02 15:07:20 +0530

    Add new line Hi

--- DemoGIT/Program.cs
+++ DemoGIT/Program.cs
@@ -11,6 +11,7 @@ namespace DemoGIT
         static void Main(string[] args)
         {
             Console.WriteLine("Hello");
+            Console.WriteLine("Hi");
             Console.ReadLine();
         }
     }
